294ebe26e8b44be8f78c90cf476312c1dcd5dd15
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-04-11 18:28:40 +0500

    yangi double

--- pdplesson6/Program.cs
+++ pdplesson6/Program.cs
@@ -8,6 +8,9 @@
         double  number =Convert .ToDouble (Console .ReadLine());    
 
         Console .WriteLine ( number );
+         double  number2 =Convert .ToDouble (Console .ReadLine());    
+
+        Console .WriteLine ( number );
 
 
 
